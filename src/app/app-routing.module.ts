990cc7bfc50bde58ec3beaf1b6ff17e0659d9fb1
zhao <zhaoyudong_0129@126.com>
2017-07-20 15:02:56 +0800

    skeleton web framework

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { HomePageComponent } from './home-page/home-page.component';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+ 
+const routes: Routes = [
+    {
+        path:'',component:HomePageComponent,
+    }
+];
+ 
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MdButtonModule,MdToolbarModule} from '@angular/material';
+import { HomePageComponent } from './home-page/home-page.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdButtonModule,
+    MdToolbarModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
